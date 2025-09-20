54980d9fb69ed8c5b375a95f660fa5f08658ffde
Thai Villaluna <thaivillaluna@gmail.com>
2025-09-20 10:31:44 -0700

    update setup script to download files

--- scripts/setup_db.sh
+++ scripts/setup_db.sh
@@ -1,4 +1,28 @@
 #!/bin/zsh
+# Ensure ../data/raw exists
+RAW_DIR="../data/raw"
+if [ ! -d "$RAW_DIR" ]; then
+    mkdir -p "$RAW_DIR"
+fi
+
+# Only download if data/raw is empty
+if [ -z "$(ls -A $RAW_DIR)" ]; then
+    ZIP_PATH="../data/raw/raw_data.zip"
+    GDRIVE_ID="1gLSw0RLjBbtaNy0dgnGQDAZOHIgCe-HH" # Oracle Elixir's game data folder
+    echo "Downloading data/raw from Google Drive..."
+    gdown --folder "https://drive.google.com/drive/u/1/folders/$GDRIVE_ID" -O "$RAW_DIR"
+
+    # If any zip files were downloaded, unzip them into data/raw. Probably unnecessary bc gdown downloads every file separately.
+    for zipfile in "$RAW_DIR"/*.zip; do
+        if [ -f "$zipfile" ]; then
+            echo "Unzipping $zipfile into $RAW_DIR"
+            unzip -o "$zipfile" -d "$RAW_DIR"
+            rm "$zipfile"
+        fi
+    done
+else
+    echo "$RAW_DIR is not empty. Skipping download."
+fi
 
 # Delete the database if it exists
 DB_PATH="../db/prometheus.db"
